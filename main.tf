a4875b9f861d1cf34e9716f7dc84cfc95715d4f8
aurorali <aurora.li615@gmail.com>
2021-09-14 09:53:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "google_container_node_pool" "gke_pool" {
   name       = "${var.cluster_name}-pool"
   location   = var.region
   cluster    = google_container_cluster.gke_cluster.name
-  initial_node_count = 3
+  initial_node_count = 1
  
   autoscaling {
     min_node_count = 0
